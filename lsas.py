bde343c7f5ca1cce6f3b028c40dfd287a79437e6
qualmluc <lucasqualmann@gmx.de>
2025-08-21 19:19:55 +0200

    feat - id to champ, item rough

---
+++ lsas.py
@@ -0,0 +1,7 @@
+import src.map as idmaps
+
+mirror_champions = 'https://ddragon.leagueoflegends.com/cdn/15.16.1/data/en_US/champion.json'
+mirror_items     = 'https://ddragon.leagueoflegends.com/cdn/15.16.1/data/en_US/item.json'
+
+print(idmaps.toChampion(84, mirror_champions))
+print(idmaps.toItem(1042, mirror_items))
---
+++ src/map.py
@@ -0,0 +1,22 @@
+import requests
+import jsonify
+
+def toChampion(id: int, Championdata_url: str):
+    dataOfChampions_response = requests.get(Championdata_url)
+    dataOfChampions_dict = dataOfChampions_response.json()
+
+    Champions_dict = dataOfChampions_dict['data']
+
+    for cname in Champions_dict:
+        if Champions_dict[cname]['key'] == str(id):
+            return cname
+
+    return ""
+
+def toItem(id: int, Itemdata_url: str):
+    dataOfItems_response = requests.get(Itemdata_url)
+    dataOfItems_dict = dataOfItems_response.json()
+
+    Items_dict = dataOfItems_dict['data']
+
+    return Items_dict[str(id)]['name']
