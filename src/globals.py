07c8de98383fa7d31869f426f76f2dba3d2f8ccf
Alexander Doering <alexander-doering@outlook.de>
2025-08-27 14:20:36 +0200

    style - documentation and moving things around

--- src/globals.py
+++ src/globals.py
@@ -1,1 +1,7 @@
 teamname = "${Team_Name}"
+roster = ['b5574b0e-0047-53f8-8c92-e91a53ad630c', '008dbe81-2932-5d47-b25f-8d60729c2485', 'abd511d9-4c29-595a-b8cb-9f23fbd0f1cc', 'a3965f7f-1a7e-53d5-94a7-fd1ec685b64b', '806137fe-85b8-5f4b-ae05-33ab7c261c64']
+#Roster(incl. old): Leon, Alex, Karsten, Felix, Markus
+    #TODO: Add Hauke, Kira, Top, Max
+    #TODO: Remove karsten
+    #TODO: Build this as a dictionary to support multi-roster.
+        #--> How to support players swapping teams ?
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 import os
-from src.globals import teamname
+from src.globals import teamname, roster
 
 def findMatchFile():
     """
@@ -15,10 +15,14 @@ def findMatchFile():
     return matchfile
 
 def playerTeamCheck(pUuid):
-    #Roster(incl. old): Leon, Alex, Karsten, Felix, Markus
-    roster = ['b5574b0e-0047-53f8-8c92-e91a53ad630c', '008dbe81-2932-5d47-b25f-8d60729c2485', 'abd511d9-4c29-595a-b8cb-9f23fbd0f1cc', 'a3965f7f-1a7e-53d5-94a7-fd1ec685b64b', '806137fe-85b8-5f4b-ae05-33ab7c261c64']
-    #TODO: Add Hauke, Kira, Top, Max
-    #TODO: Remove karsten
+    """
+    Uses a Players identifier to check if he's on a known Team.
+    ----------
+    Return
+    ----------
+        team (str):          Known teamname or "enemyteam" as placeholder- Currently only supports ${Team_Name}
+    ----------
+    """
     if pUuid in roster:
         team = teamname
     else:
