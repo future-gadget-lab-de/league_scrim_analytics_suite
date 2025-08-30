6cc62874489fe59904eedcabb09e45691d3ef20e
Alexander Döring <alexander@doering-it.eu>
2025-08-30 15:41:55 +0200

    feat - database connection draft.

---
+++ src/database.py
@@ -0,0 +1,29 @@
+import mariadb
+import sys
+
+try: #TODO: Load from Config
+    # connection parameters
+    conn_params = {
+        'user' : "",
+        'password' : "",
+        'host' : "",
+        'port' : 3306,
+        'database' : ""
+    }
+
+    # establish a connection
+    connection = mariadb.connect(**conn_params)
+    cursor = connection.cursor()
+    
+    # query
+
+    query = """INSERT INTO metadata (gameid, patch, date, duration) 
+    VALUES 
+    (7493705947, '15.16', '2025-08-13', '0:33:35')"""
+    cursor.execute(query)
+    connection.commit()S
+    cursor.close()
+
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
