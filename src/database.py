87bc552c221926c733fda7df715f7b730d1b7c32
Alexander Döring <alexander@doering-it.eu>
2025-09-04 17:37:23 +0200

    style - readbility

--- src/database.py
+++ src/database.py
@@ -2,24 +2,17 @@ import mariadb
 import sys
 
 def insertMetadata(gameid, patch, date, duration,conn_params):
-    try:
-        # establish a connection)
-        conn_params['port'] = int(conn_params['port'])
+        # establish a connection
+        conn_params['port'] = int(conn_params['port']) 
         connection = mariadb.connect(**conn_params)
         cursor = connection.cursor()
-        # query
+        # Build querey
         basequery       = "INSERT INTO metadata (gameid, patch, date, duration) VALUES"
         values          = gameid, patch, date, duration
         value_string    = str(values)
         query = basequery + " " + value_string
-        print(query)
-        cursor.execute(query)
-        connection.commit()
-        cursor.close()
-
-    except mariadb.Error as e:
-        print(f"Error connecting to MariaDB Platform: {e}")
-        sys.exit(1)
+        # Execute query
+        executeQuery(query, cursor, connection)
 
 def executeQuery(query, cursor, connection):
     try:
