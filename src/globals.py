94c3df63db84204a881b07ae8a6d3629eba75c67
Alexander Döring <alexander@doering-it.eu>
2025-12-18 20:59:32 +0000

    feat - Patch 15.24.1 Games & current roster

--- src/globals.py
+++ src/globals.py
@@ -1,8 +1,6 @@
 teamname = "${Team_Name}"
-roster = ['b5574b0e-0047-53f8-8c92-e91a53ad630c', '008dbe81-2932-5d47-b25f-8d60729c2485', 'abd511d9-4c29-595a-b8cb-9f23fbd0f1cc', 'a3965f7f-1a7e-53d5-94a7-fd1ec685b64b', '806137fe-85b8-5f4b-ae05-33ab7c261c64']
-#Roster(incl. old): Leon, Alex, Karsten, Felix, Markus
-    #TODO: Add Hauke, Kira, 
-    #TODO: Remove karsten
+roster = ['cad364cc-c655-5e5a-8547-f81933e764f4', '008dbe81-2932-5d47-b25f-8d60729c2485', 'c15ae42b-8e36-59d7-a67a-aa66dfe65052', ,'985ed00f-31a4-5119-a332-61816fe2a36a', '9d8ae6f3-47c7-5a18-aaf7-aeae30b12367' ,'b5574b0e-0047-53f8-8c92-e91a53ad630c', '806137fe-85b8-5f4b-ae05-33ab7c261c64']
+#Roster(incl. Sub): Flo, Alex, Kira, Onur, Hauke, Leon, Markus
     #TODO: Build this as a dictionary to support multi-roster.
         #--> How to support players swapping teams ?
 patch = "15.24.1"
