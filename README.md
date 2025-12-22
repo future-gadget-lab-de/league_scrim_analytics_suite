a4f1b3de839d9934978013d05f4688fbdbaa8b16
doeriale <alexander@doering-it.eu>
2025-12-22 09:32:39 +0100

    Chore | New tasks & cleanup on SQL Dump

--- README.md
+++ README.md
@@ -5,10 +5,12 @@ Highest Priority (ordered)
 - [ ] Test on Patch 16.24
 - [ ] Frontend für Datenauslese bzw. Visualisierung
 - [ ] Setup script für DB/Folder Struct {...} für Win/Linux
-
+- [ ] Refactor globals/utils and read team_metadata from .conf file
+    - [ ] Example .conf files (DB, team ...)
 Low Prio // QOL:
 - [ ] Automate getting Patchname (Maybe Webscrape ?)
     - [ ] Automatisierte Merge-Request erstellung für Patch erneuerung (Renovate Bot)
 - [ ] Queries preparen für Frontend (kann warten bzw. geht schnell // improv.)
 - [ ] Pipeline für Imports (bspw. per Owncloud oder Mail)
-
+- [ ] Proper Logging for DB-Transactions , Imports and file-moving with following convention:
+``` [DD-MM-YYYY HH:MM:SS],[LOGLEVEL], MESSAGE ```
--- config/db_creation_dump.sql
+++ config/db_creation_dump.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.3
--- https://www.phpmyadmin.net/
---
--- Host: sql572.your-server.de
--- Generation Time: Dec 18, 2025 at 09:34 PM
--- Server version: 10.11.14-MariaDB-0+deb12u2
--- PHP Version: 8.5.0
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `lsas_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `metadata`
---
 
 CREATE TABLE `metadata` (
   `gameid` bigint(20) NOT NULL,
@@ -35,7 +10,6 @@ CREATE TABLE `metadata` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `playerdata`
 --
@@ -77,7 +51,6 @@ CREATE TABLE `playerdata` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `teamdata`
 --
@@ -100,30 +73,19 @@ CREATE TABLE `teamdata` (
   `firstbr` tinyint(1) NOT NULL,
   `win` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Indexes for dumped tables
---
-
 --
 -- Indexes for table `metadata`
 --
 ALTER TABLE `metadata`
   ADD PRIMARY KEY (`gameid`);
-
 --
 -- Indexes for table `playerdata`
 --
 ALTER TABLE `playerdata`
   ADD PRIMARY KEY (`gameid`,`playerid`);
-
 --
 -- Indexes for table `teamdata`
 --
 ALTER TABLE `teamdata`
   ADD PRIMARY KEY (`gameid`,`teamid`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
