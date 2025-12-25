8d4534abaeb24c3730f4b46be8b3b76657a4ea16
lucasq <lucasqualmann@gmx.de>
2025-12-25 16:34:58 +0100

    chore - dropped unused code

--- lsas.py
+++ lsas.py
@@ -4,7 +4,6 @@ logger = logging.getLogger(__name__)
 
 import os, os.path, sys
 # gui stuff
-from src.visuals.simplefrontend import SimpleFrontend
 from src.visuals.windows.main_window import MainWindow
 from PySide6.QtWidgets import QApplication
 # mariadb stuff
@@ -14,37 +13,8 @@ from src.database.sqltemplates.template import importSQLQueries
 
 from src.utils import list_relative_filepaths
 
-def databaseSetup():
-    # Check how many Matchfiles exist
-    delete_queries = importSQLQueries("src/database/sqltemplates/db_delete_alldata.sql")
-    create_queries = importSQLQueries("src/database/sqltemplates/db_creation_dump.sql")
-
-    matchfiles = list_relative_filepaths("gamefiles/matchdata")
-
-    matchfile_queries = dict()
-    for match in matchfiles:
-        matchfile_queries[match] = returnMatchfileQuery("gamefiles/matchdata/"+match)
-
-    conn, cur = buildConnection()
-    
-    try:
-        executeQuery(delete_queries, conn, cur)
-        executeQuery(matchfile_queries[matchfiles[0]], conn, cur)
-
-    finally: 
-        logger.info("Connection to MariaDB Server closed")
-        cur.close()
-
-def runFrontend() -> None:
-    app = QApplication(sys.argv)
-    # basically will "starten" execute the passed function
-    window = SimpleFrontend(databaseSetup)
-    window.show()
-    sys.exit(app.exec())
-
 def runAdvancedFrontend() -> None:
     app = QApplication(sys.argv)
-    # basically will "starten" execute the passed function
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
@@ -52,6 +22,4 @@ def runAdvancedFrontend() -> None:
 if __name__ == "__main__":
     setup_logging(level = "DEBUG")
     runAdvancedFrontend()
-    #databaseSetup()
-    #runFrontend()
 
