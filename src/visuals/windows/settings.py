af97f437c477c539b26e37523c7c59d979b13f42
lucasq <lucasqualmann@gmx.de>
2025-12-25 13:40:45 +0100

    feat - added entry for api key

--- src/visuals/windows/settings.py
+++ src/visuals/windows/settings.py
@@ -12,13 +12,19 @@ class SettingsDialog(QDialog):
         self.ui.setupUi(self)
         
         self.settings = readSettingsFile("config/lsas.conf")
+        self.init_fields()
+
+        self.ui.lineEdit_csv_path.textChanged.connect(self.change_line_setting)
+        self.ui.lineEdit_API.textChanged.connect(self.change_line_setting)
+        self.ui.checkbox_mariadb_activated.stateChanged.connect(self.change_maria_setting)
+
+    def init_fields(self) -> None:
         if self.settings["mariadb"] == "1":
             self.ui.checkbox_mariadb_activated.setChecked(True)
             self.ui.lineEdit_csv_path.setDisabled(True)
         self.ui.lineEdit_csv_path.setText(self.settings["csv_directory"])
+        self.ui.lineEdit_API.setText(self.settings["API_key"])
 
-        self.ui.lineEdit_csv_path.textChanged.connect(self.change_csv_setting)
-        self.ui.checkbox_mariadb_activated.stateChanged.connect(self.change_maria_setting)
 
     def get_settings(self) -> dict:
         return self.settings
@@ -31,5 +37,6 @@ class SettingsDialog(QDialog):
             self.settings["mariadb"] = 0
             self.ui.lineEdit_csv_path.setEnabled(True)
 
-    def change_csv_setting(self) -> None:
+    def change_line_setting(self) -> None:
         self.settings["csv_directory"] = self.ui.lineEdit_csv_path.text()
+        self.settings["API_key"] = self.ui.lineEdit_API.text()
