7a6df09d4e1411077a18be63e42513fd30472d9d
lucasq <lucasqualmann@gmx.de>
2025-12-29 23:05:53 +0100

    chore - args improved with exclusivity

--- src/args.py
+++ src/args.py
@@ -5,10 +5,12 @@ def initiliazeParser() -> argparse.ArgumentParser:
         prog="lsas",
         description="league statistics analytics suite"
     )
-    parser.add_argument("-g", "--gui", help="runs the graphical user interface, shipped with lsas", action="store_true")
     parser.add_argument("-v", "--verbose", help="sets the logging level to DEBUG. standard is INFO.", action="store_true")
-    parser.add_argument("-f", "--matchfile", type=str, help="imports the matchfiles passed into the arg via absolute path (folder & file support)")
-    parser.add_argument("-c", "--config", type=str, help="adjusts the config folder with a passed relative path")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-f", "--matchfile", type=str, help="imports the matchfiles passed into the arg via absolute path (folder & file support)")
+    group.add_argument("-c", "--config", type=str, help="adjusts the config folder with a passed relative path")
+    group.add_argument("-g", "--gui", help="runs the graphical user interface, shipped with lsas", action="store_true")
+    
 
     return parser
 
