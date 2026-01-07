3cfa005f373dcf6a955606263c598091e8912a14
doeriale <alexander@doering-it.eu>
2026-01-07 09:52:15 +0100

    Logging for data.py

--- src/scraping/data.py
+++ src/scraping/data.py
@@ -15,14 +15,15 @@ def scrapeRecentPatch() -> str:
         the recent patch number as a string
     
     """
+    logger.trace("Started scrapeRecentPatch function.")
 
     link = "https://ddragon.leagueoflegends.com/realms/euw.json"
     date = datetime.datetime.today().strftime('%Y-%m-%d')
     data_file_path = f"src/scraping/dictionaries/EUW_{date}.json"
+
     logger.debug("Reload of the recent Patchnumber.")
-
     data_dict = reloadjsonfiles(data_file_path, link)
-
+    logger.trace("Finished scrapeRecentPatch function.")
     return data_dict["v"]
 
 def returnScrapeLink(dataRequested: str, patch: str | None = None) -> str:
@@ -42,11 +43,12 @@ def returnScrapeLink(dataRequested: str, patch: str | None = None) -> str:
     link : str
         The link according to the wanted type of data
     """
+    logger.trace("Started returnScrapeLink function for patch: " + patch)
     if patch is None:
         patch = scrapeRecentPatch()
 
     link = 'https://ddragon.leagueoflegends.com/cdn/' + patch + '/data/en_US/'
-
+    logger.debug("Generated API Link.")
     # dataRequested -> suffix
     suffixes = {
         'perk':     'runesReforged.json',
@@ -56,7 +58,7 @@ def returnScrapeLink(dataRequested: str, patch: str | None = None) -> str:
     }
 
     link += suffixes[dataRequested]
-
+    logger.trace("Finished returnScrapeLink function with output: " + link)
     return link
 
 def loadDatabase(dataRequested: str, patch: str | None = None) -> dict:
@@ -76,6 +78,7 @@ def loadDatabase(dataRequested: str, patch: str | None = None) -> dict:
     data_dict : dict
         The Dictionary, which has the wanted lol data
     """
+    logger.trace("Started function loadDatabase with dataRequested: " + dataRequested + ", for patch: " + patch)
     if patch is None:
         patch = scrapeRecentPatch()
         
@@ -83,6 +86,7 @@ def loadDatabase(dataRequested: str, patch: str | None = None) -> dict:
     logging.debug("Reload of the %s data.",dataRequested)
 
     scrape_link = returnScrapeLink(dataRequested)
-
-    return reloadjsonfiles(data_file_path, scrape_link)
+    data_output = reloadjsonfiles(data_file_path, scrape_link)
+    logger.trace("Finished loadDatabase function with output: + " str(data_output) )
+    return data_output
 
