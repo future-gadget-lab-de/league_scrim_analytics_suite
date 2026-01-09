b7da9823060083fbc241dd4aecdab82d71334bc1
lucasq <lucasqualmann@gmx.de>
2026-01-09 12:42:17 +0100

    style - comment test

--- src/log_config.py
+++ src/log_config.py
@@ -1,12 +1,19 @@
 """
 Log Level explanation:
-    Critical(50):   Errors where Program cannot continue running , example: duplicate keys in database
-    Error(40):      Failure in a function that leads to that function not being able to complete.
-    Warn(30):       Failure in the Program that needs to alert the user but not halt , example: connection failed to DB, retry possible
-    Success(25):    Log succesful larger operations.
-    Info(20):       General Purpose logs, "Start importing file xy , finished importing file xy" and so on.
-    Debug(10):      Expands to log more frequently than Info, also with the file open at the time of logging.
-    Trace(5):       Log EVERYTHING , what method is being called, what file is opened (...)
+    - Critical(50):   Errors where Program cannot continue running , example: duplicate keys in database
+
+    - Error(40):      Failure in a function that leads to that function not being able to complete.
+
+    - Warn(30):       Failure in the Program that needs to alert the user but not halt , example: connection failed to DB, retry possible
+
+    - Success(25):    Log succesful larger operations.
+
+    - Info(20):       General Purpose logs, "Start importing file xy , finished importing file xy" and so on.
+
+    - Debug(10):      Expands to log more frequently than Info, also with the file open at the time of logging.
+
+    - Trace(5):       Log EVERYTHING , what method is being called, what file is opened (...)
+    
 """
 import sys
 from loguru import logger
