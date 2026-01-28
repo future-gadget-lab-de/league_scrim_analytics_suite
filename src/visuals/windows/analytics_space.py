d84def0dad71b3c8c4919ed2bd97f8402df2c24c
qualmluc <lucasqualmann@gmx.de>
2026-01-28 22:49:29 +0100

    feat - functinoal tiling + persistance

--- src/visuals/windows/analytics_space.py
+++ src/visuals/windows/analytics_space.py
@@ -1,27 +1,84 @@
 """the wrapper class which manages the container of the Analyticsspaces."""
 from __future__ import annotations
 from loguru import logger
+import json
 
 from src.visuals.windows.analytics_singleton import AnalyticsSingleton
 from src.visuals.ui.generated.ui_scroll_wrapper import Ui_Scroll_wrapper
 
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, QKeyCombination, Signal
+from PySide6.QtGui import QKeyEvent
 
 from PySide6.QtWidgets import (QApplication, QHBoxLayout, QVBoxLayout, QSizePolicy, QVBoxLayout, 
-    QWidget, QSplitter, QLabel, QGroupBox, QPushButton)
+    QWidget, QSplitter, QLabel, QGroupBox, QPushButton, QLineEdit)
+
+from src.utils.io import writeJsonFile, readJsonFile
+
+class NeoAnalyticsSingleton(QWidget):
+    
+    buttonpressed = Signal(str, QWidget)
+    
+    def __init__(self, contained: QWidget, parent=None):
+
+        super().__init__(parent)
+        self.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
+        layout = QHBoxLayout(self)
+        self.widget = contained
+        layout.addWidget(self.widget)
+        self.setLayout(layout)
+
+
+
+
+    def keyPressEvent(self, e: QKeyEvent):
+
+        mod = Qt.KeyboardModifier.ShiftModifier
+        print(e.modifiers())
+
+        if e.modifiers() == mod:
+        
+            match e.key():
+
+                case Qt.Key.Key_Q:
+                    print("emitted")
+                    self.buttonpressed.emit("horizontal", self)
+
+                case Qt.Key.Key_E:
+                    print("emitted")
+                    self.buttonpressed.emit("vertical", self)
+
+        super().keyPressEvent(e)
+
+    def mousePressEvent(self, event):
+
+        self.setFocus()
+        super().mousePressEvent(event)
 
 class NeoAnalyticsSpace(QWidget):
     def __init__(self, parent=None):
 
         super().__init__(parent)
 
+        self.widgets = []
+        self.splitter = []
+
+        self.struct = dict()
+
+        struct_dict = readJsonFile("config/ctr.json")
+        struct_std = [
+            {
+                "id": "rootpanel",
+                "parent": "root"
+            }
+        ]
+        split = self._build_by_struct(struct_dict, "root", 1, [])
+        self.splitter.append(
+            split
+        )
+
         horizontallayout = QHBoxLayout(self)
 
-        self.splitter = QSplitter()
-        self.splitter.addWidget(self._make_panel())
-
-
-        horizontallayout.addWidget(self.splitter)
+        horizontallayout.addWidget(split)
         self.setLayout(horizontallayout)
 
 
@@ -32,22 +89,115 @@ class NeoAnalyticsSpace(QWidget):
         but2 = QPushButton("hort")
         layout.addWidget(but1)
         layout.addWidget(but2)
+        layout.addWidget(QLineEdit())
         group.setLayout(layout)
-        but1.clicked.connect(lambda checked=False, b=but1: self._add_Splitter(b, Qt.Orientation.Vertical, checked))
-        but2.clicked.connect(lambda checked=False, b=but2: self._add_Splitter(b, Qt.Orientation.Horizontal, checked))
-        return group
 
-    
-    def _add_Splitter(self, button: QPushButton, direction: Qt.Orientation, _checked: bool = False):
-        widget = button.parent()
-        splitter = widget.parent()
+        test = NeoAnalyticsSingleton(group)
+
+        but1.pressed.connect(self.saveStates)
+        test.buttonpressed.connect(self._add_Splitter_str)
+
+        #but1.clicked.connect(lambda checked=False, b=but1: self._add_Splitter(b, Qt.Orientation.Vertical, checked))
+        #but2.clicked.connect(lambda checked=False, b=but2: self._add_Splitter(b, Qt.Orientation.Horizontal, checked))
+        return test
+
+    def _build_by_struct(self, rec_dict: list[dict], splitid: str, ind: int, size: list[int]):
+
+        if ind%2 == 1:
+            split = QSplitter(orientation=Qt.Orientation.Vertical)
+        else:
+            split = QSplitter(orientation=Qt.Orientation.Horizontal)
+        
+        self.splitter.append(split)
+        split.setProperty("id", splitid)
+        self.struct[splitid] = dict()
+        self.struct[splitid]["children"] = []
+
+        for widget in rec_dict:
+            cid = widget["id"]
+            self.struct[cid] = dict()
+
+            if "children" in widget:
+                re_split = self._build_by_struct(widget["children"], cid, ind+1, widget["size"])
+                split.addWidget(re_split)
+            else:
+                newPanel = self._make_panel()
+                newPanel.setProperty("id", cid)
+                self.widgets.append(newPanel)
+                split.addWidget(newPanel)
+
+            self.struct[splitid]["children"].append(cid)
+            self.struct[cid]["parent"] = splitid
+
+
+        if size:
+            split.setSizes(size)
+
+        print(self.struct)
+        return split
+
+
+
+
+
+
+    def saveStates(self):
+        for widget in self.widgets:
+            pass # get some and save
+
+        for split in self.splitter:
+            sid = split.property("id")
+            sizes = split.sizes()
+            self.struct[sid]["size"] = sizes
+
+        baseids = self.struct["root"]["children"]
+        rec_dict = self.recursifyDict(self.struct, baseids)
+        print(rec_dict)
+        writeJsonFile(rec_dict, "config/ctr.json")
+
+    def recursifyDict(self, explicit: dict, ids: list[str]) -> dict:
+        final = list()
+        
+        for i in ids:
+            di = dict()
+            di["id"] = i 
+            for key in explicit[i]:
+                di[key] = explicit[i][key]
+            if "children" in explicit[i] and explicit[i]["children"] is not None:
+                di["children"] = self.recursifyDict(explicit, explicit[i]["children"])
+            
+            final.append(di)
+                
+
+        return final
+
+    def _add_Splitter_str(self, ident: str, wid: QWidget):
+        if ident == "horizontal":
+            direction = Qt.Orientation.Horizontal
+        else: 
+            direction = Qt.Orientation.Vertical
+
+        widget = wid
+        splitter: QSplitter = widget.parent()
 
         if not isinstance(splitter, QSplitter):
             return
         splitter_ori = splitter.orientation()
+        widget_ind = splitter.indexOf(widget)
+
+        len_of_struct = len(self.struct)
 
         if direction == splitter_ori:
-            splitter.addWidget(self._make_panel())
+            newpanel = self._make_panel()
+            newpanel.setProperty("id", f"something_{len_of_struct}")
+            print(self.struct[splitter.property("id")]["children"])
+            print(newpanel.property("id"))
+            self.struct[splitter.property("id")]["children"].append(newpanel.property("id"))
+            self.struct[newpanel.property("id")] = dict()
+            self.struct[newpanel.property("id")]["parent"] = splitter.property("id")
+            self.widgets.append(newpanel)
+            splitter.insertWidget(widget_ind, newpanel)
+            print(self.struct)
             return
 
         index = splitter.indexOf(widget)
@@ -58,11 +208,30 @@ class NeoAnalyticsSpace(QWidget):
             return
         old_widget.setParent(None)
         newSplitter = QSplitter(orientation=direction)
+        self.splitter.append(newSplitter)
+        newSplitter.setProperty("id", f"sp_{len_of_struct}")
         splitter.insertWidget(index, newSplitter)
         newSplitter.addWidget(old_widget)
-        newSplitter.addWidget(self._make_panel())
-        #newSplitter.setSizes([1, 1])
-        
+        new_widget = self._make_panel()
+        self.widgets.append(new_widget)
+        new_widget.setProperty("id", f"wi_{len_of_struct}")
+        newSplitter.addWidget(new_widget)
+        newSplitter.setSizes([1, 1])
+        splitter.setSizes([1 for size in splitter.sizes()])
+        print(widget.property("id"))
+        self.struct[splitter.property("id")]["children"].remove(widget.property("id"))
+        self.struct[splitter.property("id")]["children"].append(newSplitter.property("id"))
+
+        self.struct[newSplitter.property("id")] = dict()
+        self.struct[newSplitter.property("id")]["parent"] = splitter.property("id")
+        self.struct[newSplitter.property("id")]["children"] = [widget.property("id"), new_widget.property("id")]
+
+        self.struct[widget.property("id")]["parent"] = newSplitter.property("id")
+
+        self.struct[new_widget.property("id")] = dict()
+        self.struct[new_widget.property("id")]["parent"] = newSplitter.property("id")
+
+        print(self.struct)
 
     def _divide_active_window(self):
         pass
