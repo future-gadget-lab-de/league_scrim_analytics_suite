e96b9b0060cb28143c87421afa55187422e38ccc
qualmluc <lucasqualmann@gmx.de>
2026-01-28 03:22:27 +0100

    feat - some i3 styled shenanigans

--- src/visuals/windows/analytics_space.py
+++ src/visuals/windows/analytics_space.py
@@ -2,9 +2,71 @@
 from __future__ import annotations
 from loguru import logger
 
-from PySide6.QtWidgets import QWidget
 from src.visuals.windows.analytics_singleton import AnalyticsSingleton
 from src.visuals.ui.generated.ui_scroll_wrapper import Ui_Scroll_wrapper
+
+from PySide6.QtCore import Qt
+
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QVBoxLayout, QSizePolicy, QVBoxLayout, 
+    QWidget, QSplitter, QLabel, QGroupBox, QPushButton)
+
+class NeoAnalyticsSpace(QWidget):
+    def __init__(self, parent=None):
+
+        super().__init__(parent)
+
+        horizontallayout = QHBoxLayout(self)
+
+        self.splitter = QSplitter()
+        self.splitter.addWidget(self._make_panel())
+
+
+        horizontallayout.addWidget(self.splitter)
+        self.setLayout(horizontallayout)
+
+
+    def _make_panel(self) -> QGroupBox:
+        group = QGroupBox()
+        layout = QHBoxLayout()
+        but1 = QPushButton("vert")
+        but2 = QPushButton("hort")
+        layout.addWidget(but1)
+        layout.addWidget(but2)
+        group.setLayout(layout)
+        but1.clicked.connect(lambda checked=False, b=but1: self._add_Splitter(b, Qt.Orientation.Vertical, checked))
+        but2.clicked.connect(lambda checked=False, b=but2: self._add_Splitter(b, Qt.Orientation.Horizontal, checked))
+        return group
+
+    
+    def _add_Splitter(self, button: QPushButton, direction: Qt.Orientation, _checked: bool = False):
+        widget = button.parent()
+        splitter = widget.parent()
+
+        if not isinstance(splitter, QSplitter):
+            return
+        splitter_ori = splitter.orientation()
+
+        if direction == splitter_ori:
+            splitter.addWidget(self._make_panel())
+            return
+
+        index = splitter.indexOf(widget)
+        if index < 0:
+            return
+        old_widget = splitter.widget(index)
+        if old_widget is None:
+            return
+        old_widget.setParent(None)
+        newSplitter = QSplitter(orientation=direction)
+        splitter.insertWidget(index, newSplitter)
+        newSplitter.addWidget(old_widget)
+        newSplitter.addWidget(self._make_panel())
+        #newSplitter.setSizes([1, 1])
+        
+
+    def _divide_active_window(self):
+        pass
+
 
 class AnalyticsSpace(QWidget):
     """wrapper class for analyticsspaces
@@ -26,9 +88,12 @@ class AnalyticsSpace(QWidget):
         super().__init__(parent)
         self.ui = Ui_Scroll_wrapper()
         self.ui.setupUi(self)
-
+        self.thinker = NeoAnalyticsSpace(self)
         # initializing a list of singletons, for proper management
         self.instances = list[AnalyticsSingleton]()
+        self.ui.scrollarea_insertlayout.addWidget(self.thinker)
+
+
 
     def _remove_instance(self) -> None:
         self.ui.scrollarea_insertlayout.removeWidget(self.instances[-1])
